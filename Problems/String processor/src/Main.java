81c912fcfb23fbafc73dd74869aa4212d439fe14
euchigere <ugochigere02@gmail.com>
2020-07-28 12:39:01 +0100

    string processor

---
+++ Problems/String processor/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class StringProcessor extends Thread {
+
+    final Scanner scanner = new Scanner(System.in); // use it to read string from the standard input
+
+    @Override
+    public void run() {
+        // implement this method
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.matches("[A-Z]+")) {
+                System.out.println("FINISHED");
+                break;
+            }
+            System.out.println(str.toUpperCase());
+        }
+    }
+}
